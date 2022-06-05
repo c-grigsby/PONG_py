0930fd3a703d5483da1f8cc4707970610f8f0818
cgrigsby <cgrigsby@celerik.com>
2022-06-04 20:45:59 -0400

    feat(gameBall): bounce on paddle collision

--- src/main.py
+++ src/main.py
@@ -34,7 +34,6 @@ while game_is_on:
 
     # Detect collision with paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        print("Made Contact")
         ball.bounce_x()
 
 
