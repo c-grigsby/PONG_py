2e9b86480299f22da4e3b527015ae5a067392bc3
cgrigsby <cgrigsby@celerik.com>
2022-06-10 21:43:30 -0400

    feat(paddle): restrict bounds

--- src/paddle.py
+++ src/paddle.py
@@ -4,18 +4,22 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self, position):
+    def __init__(self, position, max_y, min_y):
         super().__init__()
         self.color("white")
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
+        self.max_y = max_y - 50
+        self.min_y = min_y + 50
 
     def go_up(self):
-        new_y = self.ycor() + 25
-        self.goto(self.xcor(), new_y)
+        if self.ycor() < self.max_y:
+            new_y = self.ycor() + 25
+            self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.ycor() - 25
-        self.goto(self.xcor(), new_y)
+        if self.ycor() > self.min_y:
+            new_y = self.ycor() - 25
+            self.goto(self.xcor(), new_y)
